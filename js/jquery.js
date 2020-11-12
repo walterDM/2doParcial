1f0efaa5d53612a14605a2716e015c4244c40d64
cgalianoSQL <you@example.com>
2020-11-11 23:49:50 -0300

    sad

---
+++ js/jquery.js
@@ -0,0 +1,17 @@
+let x = $(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos() {
+  let x = $("#resumen");
+  x.click(ocultar);
+}
+
+function ocultar() {
+  let x = $("#resumen");
+  x.hide("slow", mostrarText);
+}
+
+function mostrarText() {
+  let x = $("#info");
+  x.show("slow");
+}
--- nosotros.php
+++ nosotros.php
@@ -2,18 +2,34 @@
     require('header.php');
 ?>
 
+
+
 <!DOCTYPE html>
 <html>
+    <link rel="StyleSheet" href="js/estilo.css">
+
 <body>
 
 <center>
 <h2>Nuestro Servicio</h2>
-    <div>
-        Los nuevos costos de los paquetes de Netflix quedarán de la siguiente manera:
+  <div id="resumen" class="recuadro">
+        <p>Los nuevos costos de los paquetes de Netflix quedarán de la siguiente manera:
         Plan Básico, un dispositivo en calidad estándar: 139 pesos, desde 129 pesos.
         Plan Estándar, dos dispositivos en calidad HD: 196 pesos, desde 169 pesos.
-        Plan Premium, cuatro dispositivos en calidad UHD: 266 pesos, desde 229 pesos.
-    </div>
+        Plan Premium, cuatro dispositivos en calidad UHD: 266 pesos, desde 229 pesos...VER MAS</p>
+  </div>
+
+  <div id="info" class="recuadro">
+    <p>Los nuevos costos de los paquetes de Netflix quedarán de la siguiente manera:
+        Plan Básico, un dispositivo en calidad estándar: 139 pesos, desde 129 pesos.
+        Plan Estándar, dos dispositivos en calidad HD: 196 pesos, desde 169 pesos.
+        Plan Premium, cuatro dispositivos en calidad UHD: 266 pesos, desde 229 pesos.</p>
+    <p>Netflix tiene tres planes: Básico, por un valor mensual de $9 dólares, Estándar ($14) y Premium ($18). ¿En qué se traducen estas tarifas en términos de contenido? En nada, porque el catálogo es el mismo para todos.
+
+Las limitaciones se presentan en forma de resolución: el Básico no se transmitirá por encima de una resolución de definición SD, (Standard Definition), mientras que justamente el plan Estándar está restringido al Full HD. Si subes a Premium podrás disfrutar del material en 4K Ultra HD.
+
+Hay otra diferencia notable entre los planes Básico, Estándar y Premium, y esa es la cantidad de personas que pueden usar la misma cuenta a la vez: un solo usuario en la opción más económica, y cuatro la más cara..</p>
+  </div>
 <center>
 
 
@@ -43,6 +59,9 @@
     xhttp.send();
     }
 </script>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="js/jquery.js"></script>
 
 </body>
 </html>
